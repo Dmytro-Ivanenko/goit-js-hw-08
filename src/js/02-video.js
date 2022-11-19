7a790fd1ebff40a1a64f1b573db319f68f9280cb
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-11-19 18:11:35 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+var throttle = require("lodash.throttle");
+import Player from "@vimeo/player";
 
+const iframe = document.querySelector("iframe");
+const player = new Player(iframe);
+
+const getTimeFromStorage = () => {
+	let time = localStorage.getItem("videoplayer-current-time");
+
+	time !== null ? Number(time) : (time = 0);
+	return time;
+};
+
+// Start time from local storage
+player.setCurrentTime(getTimeFromStorage());
+
+// events
+player.on(
+	"timeupdate",
+	throttle(() => {
+		console.log("time updated!");
+
+		player.getCurrentTime().then(function (seconds) {
+			localStorage.setItem("videoplayer-current-time", seconds);
+		});
+	}, 1000),
+);
