eecec4e59845d36cd12ca651ce61a9c76fce72a4
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-11-20 00:02:18 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,21 @@
-var throttle = require("lodash.throttle");
-
-const form = document.querySelector(".feedback-form");
-const submitButton = document.querySelector(".feedback-form button");
-
-({ email, message } = form.elements);
+var throttle = require('lodash.throttle');
+debugger;
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  submitButton: document.querySelector('.feedback-form button'),
+  email: document.querySelector('[name = email]'),
+  message: document.querySelector('[name = message]'),
+};
 
 const checkStorage = () => {
-	let item = localStorage.getItem("feedback-form-state");
-	if (item === null) {
-		console.log("no data in storage");
-		return;
-	} else {
-		email.value = JSON.parse(item).email;
-		message.value = JSON.parse(item).message;
-	}
+  let item = localStorage.getItem('feedback-form-state');
+  if (item === null) {
+    console.log('no data in storage');
+    return;
+  } else {
+    refs.email.value = JSON.parse(item).email;
+    refs.message.value = JSON.parse(item).message;
+  }
 };
 
 checkStorage();
@@ -21,28 +23,28 @@ checkStorage();
 // Event handlers
 
 const feedbackData = {
-	email,
-	message,
+  email: '',
+  message: '',
 };
 
 const formInputHandler = e => {
-	feedbackData.email = email.value;
-	feedbackData.message = message.value;
+  feedbackData.email = refs.email.value;
+  feedbackData.message = refs.message.value;
 
-	localStorage.setItem("feedback-form-state", JSON.stringify(feedbackData));
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackData));
 };
 
 const submitHandler = e => {
-	e.preventDefault();
+  e.preventDefault();
 
-	localStorage.removeItem("feedback-form-state");
-	email.value = "";
-	message.value = "";
+  localStorage.removeItem('feedback-form-state');
+  refs.email.value = '';
+  refs.message.value = '';
 
-	console.log(feedbackData);
+  console.log(feedbackData);
 };
 
 // Events
 
-form.addEventListener("input", throttle(formInputHandler, 500));
-submitButton.addEventListener("click", submitHandler);
+refs.form.addEventListener('input', throttle(formInputHandler, 500));
+refs.submitButton.addEventListener('click', submitHandler);
